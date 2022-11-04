56f11ec11b0492b2597d0cfc72a63fdeab76e1e1
Evgeniy Shemenev <norfolkevgeniy@gmail.com>
2022-11-04 20:29:16 +0300

    "в процессе"

--- src/main/java/org/shemenev/Main.java
+++ src/main/java/org/shemenev/Main.java
@@ -2,7 +2,6 @@ package org.shemenev;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -21,7 +20,14 @@ public class Main {
         }
         if (args[0].equals("-path")) {
             pathToFile = Path.of(args[1]);
+
+              // -path "C:\Users\Evgeniy\Documents\java.txt"
         }
+        // обозначаем путь для файла output.txt
+        int index = args[1].lastIndexOf("\\") + 1;
+        StringBuilder stringBuilder = new StringBuilder(args[1]);
+        String pathForOutput = String.valueOf(stringBuilder.replace(index, args[1].length(), "output.txt"));
+
 
         Parser parserObj = new Parser();
         List<String> row = parserObj.parser(pathToFile);
@@ -36,5 +42,6 @@ public class Main {
         HashMap<String, String> mapForPairs = CompareWords.compareAllWords(CompareWords.list_1, CompareWords.list_2);
         System.out.println(mapForPairs);
 
+        System.out.println(pathForOutput);
     }
 }
