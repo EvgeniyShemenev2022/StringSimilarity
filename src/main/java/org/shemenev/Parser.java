c3e982983bcc9e54e0f6b9dbbdde43b7227bb3c6
Evgeniy Shemenev <norfolkevgeniy@gmail.com>
2022-11-04 12:50:48 +0300

    Изменен на вариант добавления содержимого файла в один List, для дальнейшей удобной работы и большей вариативности.

--- src/main/java/org/shemenev/Parser.java
+++ src/main/java/org/shemenev/Parser.java
@@ -3,6 +3,7 @@ package org.shemenev;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,40 +16,19 @@ import java.util.stream.Stream;
  */
 public class Parser {
 
-    public HashMap<String, List<String>> map = new HashMap<>();
 
-    /**
-     * читывает файл,
-     * Построчно заполняет HashMap значениями из файла.
-     * Число - ключ, слова после числа - значения.
-     * @param pathToFile
-     * @return HashMap<String, List<String>>
-     */
-    public HashMap<String, List<String>> parser(Path pathToFile)  {
 
-        String[] key = new String[1];
+    public List<String> parser(Path pathToFile) throws IOException {
+
+        List<String> fileEntry;
+
         try (Stream<String> lines = Files.lines(pathToFile)) {
-            lines.filter(line -> !line.isEmpty())
+            fileEntry = lines.filter(line -> !line.isEmpty())
                     .map(String::strip)
-                    .peek(System.out::println)  // убрать
-                    .forEach(line -> {
-                        if (line.length() <= 2) {   // line.matches("\\d") - почему то не работает с первым числом???
-                            key[0] = line;
-                        }
-                        if (map.containsKey(key[0])) {
-                            map.get(key[0]).add(line);
-                        } else  {
-                            map.put((key[0]), Stream.of(line).collect(Collectors.toList()));
-                        }
-                        // Удаляю цифры, которые попали в List при первом добавлении ключа.
-                        // Придумать способ элегантнее !!!
-                        if (map.get(key[0]).size() > 1 &&  map.get(key[0]).get(0).equals(key[0])  ){
-                            map.get(key[0]).remove(0);
-                        }
-                    });
-        } catch (IOException e) {
-            e.printStackTrace();
+                    .collect(Collectors.toList());
+            return fileEntry;
         }
-        return map;
     }
+
+
 }
