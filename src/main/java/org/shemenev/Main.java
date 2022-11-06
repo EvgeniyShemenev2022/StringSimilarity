3534e4c78750e3875384520192ad51e052d0e95b
Evgeniy Shemenev <norfolkevgeniy@gmail.com>
2022-11-06 13:16:12 +0300

    добавлен return

--- src/main/java/org/shemenev/Main.java
+++ src/main/java/org/shemenev/Main.java
@@ -13,35 +13,51 @@ public class Main {
 
         if (args.length == 0) {
             System.out.println("""
-                    Hello, you should specify the path to the file
+                    --
+                    --
+                    Hello, you should specify the path to the input.txt file
                     for example:
-                    C:\\\\path> java TestReader.jar -path "C:\\\\your pass\\\\input.txt"
-                    Good Luck!""");
+                    
+                    java -jar C:\\path\\StringSimilarity-1.0.jar -path "C:\\pass\\input.txt"
+                    
+                    Good Luck!
+                    
+                    """);
+            return;
         }
-        if (args[0].equals("-path")) {
-            pathToFile = Path.of(args[1]);
 
-              // -path "C:\Users\Evgeniy\Documents\java.txt"
+        try {
+            if (args[0].equals("-path")) {
+                pathToFile = Path.of(args[1]);
+
+                // обозначаем путь для файла output.txt
+                int index = args[1].lastIndexOf("\\") + 1;
+                StringBuilder stringBuilder = new StringBuilder(args[1]);
+                String pathForOutput = String.valueOf(stringBuilder.replace(index, args[1].length(), "output.txt"));
+
+                // переносим содержимое файла в List
+                Parser parserObj = new Parser();
+                List<String> row = parserObj.parser(pathToFile);
+
+                // делим список на части, сравниваем и помещаем в map
+                CompareWords.divider(row);
+                HashMap<String, String> mapForPairs = CompareWords.compareAllWords(CompareWords.list_1, CompareWords.list_2);
+
+                // записываем содержимое map в файл output.txt
+                Writer.writeToFile(pathForOutput, mapForPairs);
+
+                System.out.println("""
+                        --
+                        Output.txt was created in the same directory as input file.
+                        program completed.
+                        
+                        """);
+            } else {
+                System.out.println("Please, take sure you enter -path + path_to_input_file");
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException("Не удалось считать файл: " + pathToFile + "\\n" + e);
         }
-        // обозначаем путь для файла output.txt
-        int index = args[1].lastIndexOf("\\") + 1;
-        StringBuilder stringBuilder = new StringBuilder(args[1]);
-        String pathForOutput = String.valueOf(stringBuilder.replace(index, args[1].length(), "output.txt"));
 
-
-        Parser parserObj = new Parser();
-        List<String> row = parserObj.parser(pathToFile);
-        System.out.println(parserObj.parser(pathToFile));
-        System.out.println(parserObj.parser(pathToFile).size());
-
-         CompareWords.divider(row);
-
-        System.out.println(CompareWords.list_1);
-        System.out.println(CompareWords.list_2);
-
-        HashMap<String, String> mapForPairs = CompareWords.compareAllWords(CompareWords.list_1, CompareWords.list_2);
-        System.out.println(mapForPairs);
-
-        System.out.println(pathForOutput);
     }
 }
